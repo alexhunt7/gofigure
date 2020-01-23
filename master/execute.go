03333c5a0f22effbd9d58979547a41cf779f6166
Alex Hunt <alexh@exabeam.com>
2020-01-22 16:14:09 -0800

    CheckExec vs Exec

--- master/execute.go
+++ master/execute.go
@@ -3,11 +3,21 @@ package master
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	pb "github.com/alexhunt7/gofigure/proto"
 )
 
+func CheckExec(client *Client, args []string, timeoutSecs int) (*pb.ExecResult, error) {
+	res, err := Exec(client, args, timeoutSecs)
+	if err == nil && int(res.ReturnCode) != 0 {
+		return res, fmt.Errorf("non-zero return code: %d\n\nSTDOUT:\n%s\n\nSTDERR:\n%s", res.ReturnCode, res.Stdout, res.Stderr)
+	}
+	return res, err
+}
+
+// You probably want CheckExec instead. This one won't error for non-zero return codes
 func Exec(client *Client, args []string, timeoutSecs int) (*pb.ExecResult, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeoutSecs)*time.Second)
 	defer cancel()
@@ -17,9 +27,9 @@ func Exec(client *Client, args []string, timeoutSecs int) (*pb.ExecResult, error
 		Args:       args[1:],
 	}
 
-	res, err := client.Exec(ctx, request)
-	if err == nil && int(res.ReturnCode) != 0 {
-		return res, fmt.Errorf("non-zero return code: %d\n\nSTDOUT:\n%s\n\nSTDERR:\n%s", res.ReturnCode, res.Stdout, res.Stderr)
-	}
-	return res, err
+	return client.Exec(ctx, request)
+}
+
+func Lines(s string) []string {
+	return strings.Split(strings.TrimSpace(s), "\n")
 }
